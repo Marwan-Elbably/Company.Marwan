d49e6ce78251fc1c8fb53e1276a01cfba2b4ed0f
Marwan-Elbably <marawanelbably@gmail.com>
2025-09-23 11:25:52 +0300

    Create Create Action Ciew

--- Company.Marwan.PL/Controllers/DepartmentController.cs
+++ Company.Marwan.PL/Controllers/DepartmentController.cs
@@ -1,5 +1,7 @@
 ﻿using Company.Marwan.BLL.Interfaces;
+using Company.Marwan.BLL.Models;
 using Company.Marwan.BLL.Reposatires;
+using Company.Marwan.PL.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.Marwan.PL.Controllers
@@ -20,5 +22,41 @@ namespace Company.Marwan.PL.Controllers
            var departments= _departmentRepository.GetAll();
             return View(departments);
         }
+        [HttpGet]
+        public IActionResult Create() { 
+        
+        
+            return  View();
+        
+        }
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDto model)
+        {
+            if (ModelState.IsValid) // server side validation
+            {
+                var department = new Department()
+                {
+                    code = model.code,
+                    name = model.name,
+                    CreateAt = model.CreateAt
+
+                };
+
+              var count =  _departmentRepository.Add(department);
+
+                if (count > 0) { 
+                    
+                    
+                        return RedirectToAction(nameof(Index));
+                
+                
+                }
+            }
+
+            return View(model);
+
+        }
+
+
     }
 }
---
+++ Company.Marwan.PL/Dto/CreateDepartmentDto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.Marwan.PL.Dto
+{
+    public class CreateDepartmentDto
+    {
+        [Required(ErrorMessage ="Code is Required")]
+        public string code { get; set; }
+
+        [Required(ErrorMessage = "Name is Required")]
+        public string name { get; set; }
+
+        [Required(ErrorMessage = "CreateAt is Required")]
+        public DateTime CreateAt { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
